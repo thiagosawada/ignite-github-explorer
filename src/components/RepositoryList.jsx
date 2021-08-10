4aa53bd71813bfd569a12d9402e776d7f29a922b
Thiago <tms1_tms2@hotmail.com>
2021-08-10 16:44:22 -0300

    RepositoryList

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,15 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+        <ul>
+          <li>
+            <strong>Unform</strong>
+            <p>Forms in react</p>
+
+            <a href="">Link para o repositório</a>
+          </li>
+        </ul>
+    </section>
+  );
+}
