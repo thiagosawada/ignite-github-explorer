bbbba88c5d96351d2afdfb658fd5482a55d2e0e6
Thiago <tms1_tms2@hotmail.com>
2021-08-10 21:35:26 -0300

    Config react fast refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Usar o path em vez do caminho "src/index.jsx" para melhor compatibilidade com sistemas operacionais
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -19,19 +20,28 @@ module.exports = {
   devServer: {
     // Onde tá o conteúdo estático da aplicação
     contentBase: path.resolve(__dirname, "public"),
+    hot: true
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html")
     })
-  ],
+  ].filter(Boolean),
   module: { // Como a aplicação vai se comportar dependendo do tipo de arquivo importado
     rules: [
       {
         // Recebe uma expressão regular para saber se é um arquivo js
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel")
+            ].filter(Boolean)
+          }
+        }
       },
       {
         test: /\.scss$/,
